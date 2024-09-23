4362ebd516bfe34644dc2e3ed5e04fcd63b22e56
Martynas Vilkas <martynasvilkas510@gmail.com>
2024-09-23 11:48:55 +0300

    Added gang script with ability to purchase equipment.

---
+++ scripts/gang.js
@@ -0,0 +1,43 @@
+const COMMANDS = ["buy"];
+
+export function autocomplete(data, args) {
+	return COMMANDS;
+}
+
+/** @param {NS} ns */
+export async function main(ns) {
+	const [command, argument] = ns.args;
+
+	if (!command) {
+		ns.tprintf(`FAILED: Command is required. Commands: ${COMMANDS}`);
+		return;
+	}
+
+	if (command && !COMMANDS.includes(command)) {
+		ns.tprintf(`FAILED: Command is not supported. Commands: ${COMMANDS}`);
+		return;
+	}
+
+	switch (command) {
+		case "buy": {
+			if (!argument) {
+				ns.tprintf(`FAILED: Equipment name is required.`);
+				return;
+			}
+			if (!ns.gang.getEquipmentNames().includes(argument)) {
+				ns.tprintf(`FAILED: Equipment not found.`);
+				return;
+			}
+
+			for (const member of ns.gang.getMemberNames()) {
+				if (ns.gang.purchaseEquipment(member, argument))
+					ns.tprintf(`Successfully purchased '${argument}' for ${member}`);
+				else 
+					ns.tprintf(`Failed to purchase '${argument}' for ${member}`);
+			}
+			break;
+		}
+		default:
+			ns.tprintf(`FAILED: Command not implemented.`);
+	}
+}
